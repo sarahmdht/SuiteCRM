0dfb9d449b7cf7d556ca3bc6a32427e433e34398
sarahmdht <sarahmdht@gmail.com>
2025-06-16 17:21:29 +0300

    Browser Preview EntryPoint

---
+++ Browser-Preview-EntryPoint/preview_pdf.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('sugarEntry')) define('sugarEntry', true);
+require_once('include/entryPoint.php');
+
+$id = $_GET['id'] ?? '';
+
+$bean = BeanFactory::getBean('Contract', $id);
+if (!$bean || empty($bean->id)) {
+    echo "Invalid document ID.";
+    exit;
+}
+
+// The file is stored in SuiteCRM as: upload/<id> (no extension)
+$filepath = 'upload/' . $bean->id;
+$filename = $bean->filename ?: ($bean->id . '.pdf');
+
+if (file_exists($filepath)) {
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: inline; filename="' . $filename . '"');
+    readfile($filepath);
+    exit;
+} else {
+    echo "PDF not found at: $filepath";
+    exit;
+}
